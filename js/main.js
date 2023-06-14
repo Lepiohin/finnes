b63f5f7446cfb609afb412e0e376d6f656ee3a52
Lepiohin Yura <ffdegvxthjfr@mail.com>
2023-06-15 00:03:09 +0300

    create starting shablon

---
+++ js/main.js
@@ -0,0 +1,15 @@
+VANTA.FOG({
+    el: "#body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0xaa56fe,
+    midtoneColor: 0xfff5dd,
+    lowlightColor: 0xfe56d9,
+    baseColor: 0x56fefe,
+    blurFactor: 1,
+    zoom: 0.3,
+    speed: 0.90
+})
