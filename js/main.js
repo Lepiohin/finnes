125b388662072a676761840e63df7a6df7d873b2
Lepiohin Yura <ffdegvxthjfr@mail.com>
2023-06-16 01:23:40 +0300

    create header

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
+//animation backgraund
 VANTA.FOG({
-    el: "#body",
+    el: "#wrap",
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
     minHeight: 200.00,
     minWidth: 200.00,
-    highlightColor: 0xaa56fe,
-    midtoneColor: 0xfff5dd,
-    lowlightColor: 0xfe56d9,
-    baseColor: 0x56fefe,
-    blurFactor: 1,
-    zoom: 0.3,
-    speed: 0.90
+    highlightColor: 0xAA56FE,
+    midtoneColor: 0xFE56D9,
+    lowlightColor: 0x56FEFE,
+    baseColor: 0xFFF5DD,
+    blurFactor: 0.8,
+    zoom: 0.8,
+    speed: 1.5
+})
+
+//burger
+
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function (){
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
 })
